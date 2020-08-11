b92acdc843f251d866fd2d60e01d7b1d4514c06d
syb0799 <69428046+syb0799@users.noreply.github.com>
2020-08-11 23:23:09 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button, Progress} from 'semantic-ui-react'
 
 function App() {
-  return (
+     <div>
 <p> 안녕하세요! </p>
-
+<Button primary> 봉쥬르 </Button>
+<Progress percent = {66} indicating/>
+</div>
   );
 }
 
